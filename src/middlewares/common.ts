cda014f851793c9398750dca54a44337000b403d
Immanuel Irwin <noelimmanuel1998@gmail.com>
2021-05-05 02:34:51 +0530

    ✨ Added common functions

--- src/middlewares/common.ts
+++ src/middlewares/common.ts
@@ -5,7 +5,15 @@ import { morganMiddleware } from './morgan';
 
 export const applyCommon = (app: Application): void => {
     app.use(json());
-    app.use(cors());
+    // app.use(
+    //     cors({
+    //         origin:
+    //             process.env.ENV === 'production'
+    //                 ? RegexUtil.PRODUCTION_WILDCARD
+    //                 : RegexUtil.DEV_WILDCARD,
+    //         credentials: true,
+    //     }),
+    // );
     app.set('trust proxy', true);
     app.use(cookieParser());
     app.use(morganMiddleware);
